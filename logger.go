daf59beb99fffbc7e7f5be57f6d8c2a6f4cc49ed
Dave <dbowers88@gmail.com>
2023-10-26 06:41:53 -0700

    added global logger, and config file

---
+++ logger.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func GlobalLogger() {
+	logFile, _ := os.OpenFile("log/log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	log.SetOutput(logFile)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetPrefix("ProgramName : ")
+}
